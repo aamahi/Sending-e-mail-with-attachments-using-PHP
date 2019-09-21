a8801a3df70533cd2fc312301b49073bfb22df9d
Abdullah al Mahi <amahi.wp@gmail.com>
2019-09-22 03:30:35 +0600

    Add files via upload

---
+++ attachmail.php
@@ -0,0 +1,37 @@
+<?php 
+$to = "amahi.wp@gmail.com";
+$from="wedevs@example.com";
+$subject = "Web Application Develoaper\n";
+$mainMessage ="Softwear Engineer\r\n";
+$fileName="./mahi.pdf";
+$fileNewName ="cv.pdf";
+
+$fileData = file_get_contents($filename);
+$boundary = "Hear is Boundary";
+
+$headers = "From :{$from}";
+$headers .= "MIME-Version :1.0 \r\n";
+$headers .= "Content-type:multipart/mixed; \r\n";
+$headers .= "boundary = \"{$boundary}\";\r\n";
+
+$message = "This is Multipart-message\r\n";
+$message .= "--{$boundary}\r\n";
+$message .= "Content-type:text/plain; charset = UTF-8;\r\n";
+$message .= "Content-Transfar-Encodeing : 7bit \r\n";
+$message .= $mainMessage."\r\n";
+
+$encodeFileData =  chunk_split(base64_encode($fileData));
+$message .= "--{$boundary}\r\n";
+$message .= "Content-type:aaplication/pdf\r\n";
+$message .= "name = {$fileName} \r\n";
+$message .= "Content-Transfar-Encodeing :base64 \r\n";
+$message .= $encodeFileData ."\r\n";
+$message .= "--{$boundary}--\r\n";
+
+$mailsend = mail($to,$subject,$message,$headers);
+if(!$mailsend){
+    die("Sending mail Failed\n");
+    exit();
+}else {
+    echo "Mail send Sucessfully\n";
+}
